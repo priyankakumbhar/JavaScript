25217124df3c48d317df03b0530ad12d94750873
priyankakumbhar <priyankakumbhar567@gmail.com>
2020-05-21 23:40:27 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,18 @@
+let firstName1='John';
+let age=25;
+if(age<13)
+{
+    console.log(firstName1+' is a boy');
+}
+else if(age>=13 && age<20)
+{
+    console.log(firstName1+ ' is a teanager');
+}
+else if(age>=20 && age<30)
+{
+    console.log(firstName1+ ' is a younger')
+}
+else
+{
+    console.log(firstName1+' is a man');
+}
---
+++ statement.js
@@ -0,0 +1,8 @@
+let isMarried=true;
+if(isMarried)
+{
+    console.log(firstName+' is married');
+}
+else{
+    console.log(firstName+' is '+civilStatus);
+}
