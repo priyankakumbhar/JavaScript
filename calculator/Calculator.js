8fa62e09171ae2c6eda7236cd18be06c8db9def1
priyankakumbhar <priyankakumbhar567@gmail.com>
2020-05-21 23:26:02 +0530

    Add files via upload

---
+++ calculator/Calculator.js
@@ -0,0 +1,37 @@
+
+function addition()
+{
+    let n1,n2,n3;
+     n1=(Number)(document.getElementById("one").value);
+     n2=(Number)(document.getElementById("two").value);
+     n3=n1+n2;
+    document.getElementById("third").value=n3;
+    
+}
+function substration()
+{
+    let n1,n2,n3;
+     n1=(Number)(document.getElementById("one").value);
+     n2=(Number)(document.getElementById("two").value);
+     n3=n1-n2;
+    document.getElementById("third").value=n3;
+    
+}
+function multiplication()
+{
+    let n1,n2,n3;
+     n1=(Number)(document.getElementById("one").value);
+     n2=(Number)(document.getElementById("two").value);
+     n3=n1*n2;
+    document.getElementById("third").value=n3;
+    
+}
+function division()
+{
+    let n1,n2,n3;
+     n1=(Number)(document.getElementById("one").value);
+     n2=(Number)(document.getElementById("two").value);
+     n3=n1/n2;
+    document.getElementById("third").value=n3;
+    
+}
