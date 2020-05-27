c370f039c68716d634e719af44018783d86b2dcd
priyankakumbhar <priyankakumbhar567@gmail.com>
2020-05-28 01:10:40 +0530

    Add files via upload

---
+++ DOM_Event/dom-event.js
@@ -0,0 +1,9 @@
+let color=['red','pink','blue','black','purple'];
+function changeColor(color)
+{
+        for(let i=0; i<color.length ;i++)
+        {
+                document.body.style.backgroundColor=color[i];        
+        }
+}
+
